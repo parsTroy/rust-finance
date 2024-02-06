ac832fec146508afb11588e713d9bc35eb37d9b6
parsTroy <troyparsons@hotmail.com>
2024-02-06 17:01:15 -0500

    Implement years and rate input

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,25 @@ const INVEST: f64 = 0.245;
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
+    
     ui.on_calculate_salary({
         let ui_handle = ui.as_weak();
-        move |string| {
+        move |salary, rate, years| {
             let ui = ui_handle.unwrap();
-            let num: f64 = string.trim().parse().unwrap();
+            let num: f64 = salary.trim().parse().unwrap();
             let tax: f64 = num * TAX_CLAW;
             let need: f64 = num * NEEDS;
             let want: f64 = num * WANTS;
             let invest: f64 = num * INVEST;
-            let result = format!("Taxes: {:.2}\nNeeds: {:.2}\nWants: {:.2}\n Invest: {:.2}", tax, need, want, invest);
+
+            let rate_percent: f64 = rate.trim().parse().unwrap();
+            let time_horizon: f64 = years.trim().parse().unwrap();
+
+            let result = format!("Taxes: {:.2}\nNeeds: {:.2}\nWants: {:.2}\n Invest: {:.2}\n RATE: {:.2}\n YEARS: {:.2}", tax, need, want, invest, rate_percent, time_horizon);
             ui.set_results(result.into());
         }
     });
+    
 
     ui.run()
 }
