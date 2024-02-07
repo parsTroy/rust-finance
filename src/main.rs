8b038cf01ba17ad1ba57531ad1b4644c32785a25
parsTroy <troyparsons@hotmail.com>
2024-02-06 19:18:14 -0500

    Calc not working inputs all working

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,32 @@ fn main() -> Result<(), slint::PlatformError> {
     
     ui.on_calculate_salary({
         let ui_handle = ui.as_weak();
-        move |salary, rate, years| {
+        move |salary, initial, rate, years| {
             let ui = ui_handle.unwrap();
             let num: f64 = salary.trim().parse().unwrap();
             let tax: f64 = num * TAX_CLAW;
             let need: f64 = num * NEEDS;
             let want: f64 = num * WANTS;
-            let invest: f64 = num * INVEST;
+            let invest: f64 = num * INVEST; // AMOUNT TO INVEST
 
-            let rate_percent: f64 = rate.trim().parse().unwrap();
-            let time_horizon: f64 = years.trim().parse().unwrap();
+            let initial_value: f64 = initial.trim().parse().unwrap(); // INITIAL VALUE
 
-            let result = format!("Taxes: {:.2}\nNeeds: {:.2}\nWants: {:.2}\n Invest: {:.2}\n RATE: {:.2}\n YEARS: {:.2}", tax, need, want, invest, rate_percent, time_horizon);
+            let rate_percent: f64 = rate.trim().parse().unwrap(); // RATE WHOLE NUMBER
+
+            let time_horizon: f64 = years.trim().parse().unwrap(); // YEARS INVESTED
+
+            fn calculate_future_value(initial_value: f64, invest: f64, rate_percent: f64, time_horizon: f64) -> f64 {
+
+                let months_invested = time_horizon as f64 * 12.0;
+                let monthly_return_rate = rate_percent / 12.0 / 100.0; // CONVERT TO DECIMAL AND COMPOUND MONTHLY
+                let future_value = initial_value * (1.0 + monthly_return_rate).powf(months_invested) + invest * ((1.0 + monthly_return_rate).powf(months_invested) - 1.0) / monthly_return_rate;
+
+                future_value
+            }
+
+            let future_value = calculate_future_value(initial_value, invest, rate_percent, time_horizon);
+
+            let result = format!("Taxes: {:.2}\nNeeds: {:.2}\nWants: {:.2}\n Invest: {:.2}\n Future Value: {:.2}", tax, need, want, invest, future_value);
             ui.set_results(result.into());
         }
     });
